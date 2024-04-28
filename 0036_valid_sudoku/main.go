6ed1a6e0b1305008f2510beebf37bef815c42fb7
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-04-28 20:40:57 +0700

    finish valid sudoku problem

--- 0036_valid_sudoku/main.go
+++ 0036_valid_sudoku/main.go
@@ -1,9 +1,39 @@
 package main
 
 func main() {
-	
+
 }
 
 func isValidSudoku(board [][]byte) bool {
-    
+	xSet := make([]map[byte]bool, 9)
+	ySet := make([]map[byte]bool, 9)
+	squareSet := make([]map[byte]bool, 9)
+
+	for i := 0; i < 9; i++ {
+		xSet[i] = make(map[byte]bool)
+		ySet[i] = make(map[byte]bool)
+		squareSet[i] = make(map[byte]bool)
+	}
+
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			currentValue := board[i][j]
+			row := i
+			col := j
+			boxIndex := (row/3)*3 + (col / 3)
+
+			if currentValue == '.' {
+				continue
+			}
+
+			if xSet[row][currentValue] || ySet[col][currentValue] || squareSet[boxIndex][currentValue] {
+				return false
+			}
+
+			xSet[row][currentValue] = true
+			ySet[row][currentValue] = true
+			squareSet[row][currentValue] = true
+		}
+	}
+	return true
 }
