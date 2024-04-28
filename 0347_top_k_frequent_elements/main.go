9cf09d2b0edca8bffd3aee577fc8a47a897d2238
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-04-28 18:43:12 +0700

    solving the top k frequent elements problem with the time complexity of O(n log k)

--- 0347_top_k_frequent_elements/main.go
+++ 0347_top_k_frequent_elements/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"container/heap"
+	"fmt"
+)
 
 func main() {
 	case1 := []int{1, 1, 1, 2, 2, 3}
@@ -8,25 +11,97 @@ func main() {
 	fmt.Println(result)
 }
 
+// func topKFrequent(nums []int, k int) []int {
+// 	m := make(map[int]int)
+
+// 	for _, num := range nums {
+// 		m[num]++
+// 	}
+
+// 	hashMap := make(map[int][]int)
+
+// 	for v, c := range m {
+// 		hashMap[c] = append(hashMap[c], v)
+// 	}
+
+// 	result := make([]int, 0)
+// 	for i := len(nums); i >= 1; i-- {
+// 		result = append(result, hashMap[i]...)
+// 		if len(result) > k {
+// 			break
+// 		}
+// 	}
+// 	return result[:k]
+// }
+
 func topKFrequent(nums []int, k int) []int {
-	m := make(map[int]int)
+	// Initialize the result slice and count map to count frequency of each element
+	var result []int
+	counterMap := make(map[int]int)
 
-	for _, num := range nums {
-		m[num]++
+	// frequency count of each element
+	for _, v := range nums {
+		counterMap[v]++
 	}
 
-	hashMap := make(map[int][]int)
+	// Initialize a heap of elements with their counts
+	h := &Heap{}
 
-	for v, c := range m {
-		hashMap[c] = append(hashMap[c], v)
-	}
+	for key, v := range counterMap {
+		e := element{
+			val: key,
+			cnt: v,
+		}
 
-	result := make([]int, 0)
-	for i := len(nums); i >= 1; i-- {
-		result = append(result, hashMap[i]...)
-		if len(result) > k {
-			break
+		// Pushing each element and its frequency into a heap
+		heap.Push(h, e)
+
+		// If heap size goes beyond k, pooping out the latest frequent one
+		if h.Len() > k {
+			heap.Pop(h)
 		}
 	}
-	return result[:k]
+
+	// Pooping remaining elements in heap and appending them to the result
+	for h.Len() > 0 {
+		result = append(result, heap.Pop(h).(element).val)
+	}
+
+	// Return the result slice when k most frequent elements from nums
+	return result
+}
+
+// Defining struct to hold element and its count
+type element struct {
+	val int
+	cnt int
+}
+
+// Defining Heap as slice of elements
+type Heap []element
+
+// Function to return len of heap
+func (h Heap) Len() int {
+	return len(h)
+}
+
+// Function to swap elements at index i and j
+func (h Heap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+// Function returning true if frequency of element at i is less than at j
+func (h Heap) Less(i, j int) bool {
+	return h[i].cnt < h[j].cnt
+}
+
+// Function to push an element into heap
+func (h *Heap) Push(x interface{}) {
+	*h = append(*h, x.(element))
+}
+
+// Function to pop out an element from heap
+func (h *Heap) Pop() (x interface{}) {
+	*h, x = (*h)[:h.Len()-1], (*h)[h.Len()-1]
+	return
 }
